9740d892cc9a87120663718c0eaca2091069a04d
yevgeniy.shaidur <insight2pizzza@gmail.com>
2022-04-27 15:13:09 +0300

    first commit

---
+++ src/main/java/com/fds/app/FdsAppApplication.java
@@ -0,0 +1,13 @@
+package com.fds.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FdsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fds/app/FdsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fds.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
