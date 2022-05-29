dfb3767f0c43e369b0189e5ad71586d4f76154a4
yevgeniy.shaidur <insight2pizzza@gmail.com>
2022-05-29 16:31:08 +0300

    implement component

--- src/main/java/com/fds/app/FdsAppApplication.java
+++ src/main/java/com/fds/app/FdsAppApplication.java
@@ -1,20 +1,15 @@
 package com.fds.app;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackages = "com.fds.app.repository")
-public class FdsAppApplication implements CommandLineRunner {
+public class FdsAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FdsAppApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws InterruptedException {
-
-    }
 }
--- src/main/java/com/fds/app/iexapis/TradeCompaniesReceiver.java
+++ src/main/java/com/fds/app/iexapis/TradeCompaniesReceiver.java
@@ -25,7 +25,7 @@ public class TradeCompaniesReceiver {
     public List<TradeCompany> getActiveCompanies() {
         List<TradeCompany> list = getTradeCompanies()
                 .stream()
-                .filter(c -> c.getIsEnabled().equals("true")).limit(4)
+                .filter(c -> c.getIsEnabled().equals("true")).limit(2000)
                 .collect(Collectors.toList());
 
         return list;
